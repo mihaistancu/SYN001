7474b019e0c8ac7a48fdaa1e6e95fb2eb505cc45
mihai stancu <stancu.mihai.dorian@gmail.com>
2021-04-13 15:32:10 +0200

    Validity end is never actually used

---
+++ Serializer.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SYN001.Tests
+{
+    class Serializer
+    {
+        public static void Serialize<T>(T data, Stream output)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("sbdh", Namespaces.Sbdh);
+            namespaces.Add("xsi", Namespaces.Xsi);
+
+            var settings = new XmlWriterSettings
+            {
+                Indent = true
+            };
+
+            using (var writer = XmlWriter.Create(output, settings))
+            {
+                serializer.Serialize(writer, data, namespaces);
+            }
+        }
+    }
+}
